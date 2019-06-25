5f1765c6e0321f2a503f136a01de0ea7fe6a0f6e
Olivia Palmu <oliviaspalmu@gmail.com>
2019-06-25 10:01:32 +0900

    global checks

--- bot.py
+++ bot.py
@@ -13,10 +13,22 @@ setup = {
 bot = commands.Bot(**setup)
 
 # Loads the bot modules
-modules = ["nuclear"]
+modules = ["cogs.nuclear"]
 if __name__ == "__main__":
     for cog in modules:
         bot.load_extension(cog)
+
+# Adds a global check to all commands.
+# Invoker must have administratir permissions and be in a guild channel (outside a DM & group channel).
+@bot.check
+def guild_and_admin(ctx):
+    if not isinstance(ctx.channel, discord.abc.GuildChannel):
+        return False
+        print("na")
+    if not ctx.channel.permissions_for(ctx.author).administrator:
+        return False
+    return True
+
 
 # Begins the bot
 bot.run(config.get("token"))
--- cogs/nuclear.py
+++ cogs/nuclear.py
@@ -1,9 +1,14 @@
 import discord
 from discord.ext import commands
 
-class NuclearCog(commands.Cog, name="nuclear"):
+# For the "nuclear fallout" armageddon option
+class NuclearCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    async def nuclear(selft, ctx):
+        print("nuclear")
 
 def setup(bot):
     bot.add_cog(NuclearCog(bot))
