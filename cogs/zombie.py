b58d513c5cb0590ab9dabbbeffbd53867b43e433
Olivia Palmu <oliviaspalmu@gmail.com>
2019-06-27 18:08:50 +0900

    made the zombie outbreak safe-ish for testing + added real webhooks

--- cogs/zombie.py
+++ cogs/zombie.py
@@ -43,9 +43,10 @@ class ZombieCog(commands.Cog):
             
             # Turns the roles "zombie"
             color = discord.Color(0x376640)
-            permissions = discord.Permissions(read_messages=True, read_message_history=True)
+            permissions = discord.Permissions(permissions=66560)
             for role in all_roles:
-                await role.edit(name="Zombie", color=color, permissions=permissions, reason="It's the end of the world!")
+                print(role.name + " turned into a zombie")
+                # await role.edit(name="Zombie", color=color, permissions=permissions, reason="It's the end of the world!")
 
             # Selects text channels for the outbreak
             text_channels = [channel for channel in ctx.guild.channels if isinstance(channel, discord.TextChannel)]
@@ -68,15 +69,21 @@ class ZombieCog(commands.Cog):
                     zombie_webhooks.append(webhook)
 
                 # The channel is now "infected"
-                await channel.edit(name="infected", slowmode_delay=21600)
+                print(channel.name + " is now infected")
+                # await channel.edit(name="infected", slowmode_delay=21600)
 
                 zombie_messages = ["grrrr....", "Braaaains...", "Grr..", "Raawr...", "...", "zzzzzz...", "Braainz...", "RRRrrr..."]
 
-                for i in range(random.randint(1, 50)):
-                    message = random.choice(zombie_messages)
-                    i = random.randint(0, 9)
-                    await zombie_webhooks[i].send(message)
-                    await asyncio.sleep(0.1)
+            zombie_webhooks = {}
+
+            for i in range(random.randint(1, 250)):
+                channel = random.choice(marked_channels)
+                if zombie_webhooks.get(channel.id) is None:
+                    zombie_webhooks[channel.id] = await channel.webhooks()
+                zombie = random.choice(zombie_webhooks[channel.id])
+                message = random.choice(zombie_messages)
+                await zombie.send(message)
+                await asyncio.sleep(0.1)
 
 
 
